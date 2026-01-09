abb81f5a52664d39b8af5c7a003334048fbcca6a
Soumil Paranjpay <soumil.paranjpay@gmail.com>
2026-01-08 22:27:28 -0800

    feat: initial site commit

---
+++ eleventy.config.js
@@ -0,0 +1,48 @@
+import fs from 'fs';
+import path from 'path';
+
+import cssnano from 'cssnano';
+import postcss from 'postcss';
+import tailwindcss from '@tailwindcss/postcss';
+
+export default function (eleventyConfig) {
+  //compile tailwind before eleventy processes the files
+  eleventyConfig.on('eleventy.before', async () => {
+    const tailwindInputPath = path.resolve('./src/assets/styles/index.css');
+
+    const tailwindOutputPath = './dist/assets/styles/index.css';
+
+    const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
+
+    const outputDir = path.dirname(tailwindOutputPath);
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    const result = await processor.process(cssContent, {
+      from: tailwindInputPath,
+      to: tailwindOutputPath,
+    });
+
+    fs.writeFileSync(tailwindOutputPath, result.css);
+  });
+
+  const processor = postcss([
+    //compile tailwind
+    tailwindcss(),
+
+    //minify tailwind css
+    cssnano({
+      preset: 'default',
+    }),
+  ]);
+
+  // Passthrough only static assets that don't get processed by Tailwind
+  // to avoid overwriting the compiled CSS in dist/assets/styles.
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+
+
+  return {
+    dir: { input: 'src', output: 'dist' },
+  };
+}
